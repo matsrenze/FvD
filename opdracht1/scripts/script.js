3bb21284c691dc933dca298c0fab4af216553676
matsrenze <mats0503@icloud.com>
2023-05-17 16:32:22 +0200

    change

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -2,8 +2,8 @@
 const circle = document.querySelector("span:nth-of-type(3)");
 
 circle.addEventListener("animationend", () => {
-    console.log("Animation ended");
-    circle.style.animation = 'rotate 4s linear infinite';
-  });
+  console.log("Animation ended");
+  circle.style.animation = "rotate 4s linear infinite";
+});
 
 // circle.style.animation = 'roll 4s ease-in'; // Start a new animation
