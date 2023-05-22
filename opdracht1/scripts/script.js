6ae2a74866ecbfffd6e926916368c20016ebf3c3
matsrenze <mats0503@icloud.com>
2023-05-22 16:46:14 +0200

    Changes | 22 May

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -1,13 +1,15 @@
 // JavaScript Document
 const circle = document.querySelector("span:nth-of-type(3)");
 const logo = document.querySelector("h1");
+const Margiela = document.querySelector("span:nth-of-type(4)");
 
 circle.addEventListener("animationend", () => {
   circle.style.animation = "rotate 4s linear infinite";
-  console.log("Animation ended");
+  console.log("Rolling End");
 });
 
-logo.addEventListener("click", () => {
-  circle.style.animation = "roll 4s ease-in-out";
+circle.addEventListener("click", () => {
+circle.style.animation = "roll 4s ease-in-out";
+Margiela.style.animation = "Carrousel 4s ease-in infinite";
   console.log("Click!");
 });
