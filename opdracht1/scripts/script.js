0a4c240fe63f342416022bbace131730d7b7ca7c
matsrenze <mats0503@icloud.com>
2023-05-23 14:43:52 +0200

    changes

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -1,26 +1,25 @@
 // JavaScript Document
 const circle = document.querySelector("span:nth-of-type(3)");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 const container = document.querySelector("h1");
 const Margiela = document.querySelector("span:nth-of-type(4)");
 
-
 circle.addEventListener("animationend", () => {
   circle.style.animation = "rotate 4s linear infinite";
-  console.log("Rolling End");
+  Margiela.style.animation = "Carrousel 5s ease-in infinite";
+  container.style.overflowX = "hidden";
+  console.log("Flipped!");
 });
 
-body.addEventListener("click", () => {
+/* body.addEventListener("click", () => {
 
-circle.style.animation = "roll 4s ease-in-out";
+circle.style.animation = "flip 2s ease-in-out";
 
  setTimeout(() => {
   Margiela.style.animation = "Carrousel 4s ease-in infinite";
-  container.style.overflow = "hidden";
+  container.style.overflowX = "hidden";
 console.log("Timer done");
-}, 3000); 
+}, 2000); 
 
   console.log("Click!");
-});
-
-
+}); */
